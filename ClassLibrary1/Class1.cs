15e8cf37f21c5f176fdc0ea815ec3ba0d8637e08
Denys <navrotskyidenys@gmail.com>
2022-10-24 01:19:19 +0300

    Modified Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,31 +1,47 @@
-﻿public class JomoPipi
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class JomoPipi
 {
 
     public static string jumbledString(string s, long n)
     {
         int count = 0;
-        List<string> evens = new List<string>();
-        List<string> odds = new List<string>();
-        
-        while (count!=n)
+        List<char> chars = s.ToCharArray().ToList();
+        List<char> evens;
+        List<char> odds;
+
+        while (count != n)
         {
-            for(int i=0; i<s.ToCharArray().Length; i++)
-            {
-                if(i%2==0)
-                {
-                    evens.Add(s[i].ToString());
-                }
-                else
-                {
-                    odds.Add(s[i].ToString());
-                }
-            }
-            s = string.Join("", evens);
-            s += String.Join("", odds);
+            evens = chars.Where((c, i) => i % 2 == 0).ToList();
+            odds = chars.Where((c, i) => i % 2 != 0).ToList();
+            chars = new List<char>();
+            chars.AddRange(evens);
+            chars.AddRange(odds);
             count++;
-            evens.Clear();
-            odds.Clear();
         }
+        s = new string(chars.ToArray());
+
+        //while (count!=n)
+        //{
+        //    for(int i=0; i<s.ToCharArray().Length; i++)
+        //    {
+        //        if(i%2==0)
+        //        {
+        //            evens.Add(s[i].ToString());
+        //        }
+        //        else
+        //        {
+        //            odds.Add(s[i].ToString());
+        //        }
+        //    }
+        //    s = string.Join("", evens);
+        //    s += String.Join("", odds);
+        //    count++;
+        //    evens.Clear();
+        //    odds.Clear();
+        //}
         return s;
     }
 
